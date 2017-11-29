1320dd04d945853ba76a20074a536610a7577ed9
book <book@computer1.(none)>
2017-11-29 10:53:13 +0800

    实时接受窗口改变的信号

---
+++ chapter18/winsize.c
@@ -0,0 +1,29 @@
+#include "apue.h"
+#include "termios.h"
+#include "sys/ioctl.h"
+
+void pr_winsize(int fd)
+{
+	struct winsize size;
+	if(ioctl(fd, TIOCGWINSZ, (char *) &size) < 0)
+		err_sys("TIOCGWINSZ error");
+	printf("%d rows, %d columns\n", size.ws_row, size.ws_col);
+}
+
+void sig_winch(int signum)
+{
+	printf("SIGWINCH received\n");	
+	pr_winsize(STDIN_FILENO);
+}
+
+int main(void)
+{
+	if(isatty(STDIN_FILENO) == 0)
+		exit(1);
+	pr_winsize(STDIN_FILENO);
+	for( ; ; ){
+		if(signal(SIGWINCH, sig_winch) == SIG_ERR)
+			err_sys("wignal error");
+		pause();
+	}
+}
